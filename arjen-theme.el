899c0c95d9bc4c8e6d55b4ab064008dbe1ef74cc
Syohei YOSHIDA <syohex@gmail.com>
2013-10-13 21:25:03 +0900

    Fix copyright(#2)

--- arjen-theme.el
+++ arjen-theme.el
@@ -1,7 +1,6 @@
 ;;; arjen-theme.el --- arjen theme
 
-;; Copyright (C) 2005, 2006  Xavier Maillard <zedek@gnu.org>
-;; Copyright (C) 2005, 2006  Brian Palmer <bpalmer@gmail.com>
+;; Copyright (C) Copyright (C) 2001 -- 2008  Arjen Wiersma
 ;; Copyright (C) 2013 by Syohei YOSHIDA
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
