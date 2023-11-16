670772e100a3b1c97e03f373b0981f9058c3b121
Martin Marshall <law@martinmarshall.com>
2023-11-16 10:10:00 -0500

    subdued-theme: Change :nil foreground values to unspecified

--- subdued-theme.el
+++ subdued-theme.el
@@ -69,8 +69,8 @@
  '(show-paren-mismatch-face ((t (:foreground "#2e3436" :background "#ef2929"))))
 
  ;; Line highlighting
- '(highlight ((t (:background "#1f1f1f" :foreground nil))))
- '(highlight-current-line-face ((t (:background "#1f1f1f" :foreground nil))))
+ '(highlight ((t (:background "#1f1f1f" :foreground unspecified))))
+ '(highlight-current-line-face ((t (:background "#1f1f1f" :foreground unspecified))))
 
  ;; Calendar
  '(holiday-face ((t (:foreground "#cc0000")))) ; dark scarlet red
